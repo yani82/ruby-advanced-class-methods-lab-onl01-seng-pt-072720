306d25708f2f09662ec4aa55feffdfd6d18e9355
Yani Asari <yani.iman1@gmail.com>
2020-08-20 05:51:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -56,6 +56,12 @@ class Song
     song
   end
   
-  
+  def self.create_from_filename(filename)
+    result = self.new_from_filename(filename)
+    song = self.create
+    song.name = result.name
+    song.artist_name = result.artist_name
+    song
+  end
 
 end

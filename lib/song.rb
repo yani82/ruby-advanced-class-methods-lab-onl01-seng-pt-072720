8794e15e6ecfab1ac14cd2f9dee6f19a659955b7
Yani Asari <yani.iman1@gmail.com>
2020-08-20 05:22:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,20 @@ class Song
   def save
     self.class.all << self
   end
+  
+  def self.create 
+    song = self.new 
+    self.all << song 
+    song 
+  end 
+  
+  def self.new_by_name(title)
+    song = self.create 
+    song.name = title 
+    song 
+  end 
+   
+  def self.find_or_create_by_name(title) 
+  end
 
 end

8dcad52e88ecdba451c8e488893c7835b5780fc5
Yani Asari <yani.iman1@gmail.com>
2020-08-20 05:48:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -47,6 +47,13 @@ class Song
     sorted
   end 
   
-  
+  def self.new_from_filename(filename)
+    song_array = filename.split(" - ")
+    song_array[1] = song_array[1].chomp(".mp3")
+    song = self.new
+    song.name = song_array[1]
+    song.artist_name = song_array[0]
+    song
+  end
 
 end
